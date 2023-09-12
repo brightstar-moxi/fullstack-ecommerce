e3946d68fd90452f90168494381e58c4512a260a
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-12 04:05:39 +0100

    update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { adminNavOptions,navOptions } from "@/utils";
+import { adminNavOptions,navOptions, styles } from "@/utils";
 import { Fragment } from "react";
 const isAdminView = false;
 const isAuthUser = false;
@@ -53,18 +53,42 @@ export default function Navbar() {
                     <div className="flex md:order-2 gap-2 ">
                         {!isAdminView && isAuthUser ? (
                             <Fragment>
-                                <button>Account</button>
-                                <button>Cart</button>
+                                <button className={styles.button}>Account</button>
+                                <button className={styles.button}>Cart</button>
                             </Fragment>
                         ) : null}
                         {
                             user?.role === 'admin' ?
-                                isAdminView ? <button>Client View</button> : <button>Admin Veiw</button>
+                                isAdminView ? <button className={styles.button}>Client View</button> : <button className={styles.button}>Admin Veiw</button>
                                 : null
                         }
                         {
-                            isAuthUser ? <button>Logout</button> : <button>Login</button>
+                            isAuthUser ? <button className={styles.button}>Logout</button> : <button className={styles.button}>Login</button>
                         }
+                           <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            //   onClick={() => setShowNavModal(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+         
                     </div>
                     <NavItem/>
                 </div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -237,4 +237,10 @@ export const navOptions = [
       label: "Postal Code",
       componentType: "input",
     },
+   
   ];
+
+  export const styles ={
+    button:
+    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white",
+};
