c3b3304b0c19f80a3baa777afa79d2b09b889107
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-13 05:33:26 +0100

    update

---
+++ src/components/CommonModal/index.js
@@ -0,0 +1,23 @@
+"use client"
+import {Transition} from "@headlessui/react"
+import {Fragment} from "react"
+
+export default function CommonModal(){
+
+    return(
+        <Transition.Root as={Fragment}>
+            <Dialog as="div" className={'relative z-10'}>
+                <Transition.Child 
+                as={Fragment}
+                enter="ease-in-out duration-900"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease"
+                >
+
+                </Transition.Child>
+                </Dialog> 
+
+        </Transition.Root>
+    )
+}
