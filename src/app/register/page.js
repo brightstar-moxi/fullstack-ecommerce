8bde38fe8e1d7256cf41fa91225f7b165cfd5a80
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-17 04:17:52 +0100

    update

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -6,7 +6,9 @@ return(
     <div className="bg-white relative">
  <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto max-w-7xl xl:px-5 lg:flex-row">
      <div className="flex flex-col justify-center items-center w-full pr-10 pl-10 lg:flex-row">
-         <div className="w-full mt-10 mr--0 mb-0 relative"></div>
+         <div className="w-full mt-10 mr-0 mb-0 relative max-w-2xl lg:mt-0 lg:w-5/12">
+             <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10"></div>
+         </div>
      </div>
  </div>
     </div>
