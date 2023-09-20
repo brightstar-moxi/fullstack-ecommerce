3ca4e2249bb1378eccbd305979942debd28c7f0a
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-20 01:46:59 +0100

    update

--- src/database/index.js
+++ src/database/index.js
@@ -10,7 +10,7 @@ const configOptions = {
 const connectToDB = async()=>{
   const connectionUrl='mongodb+srv://jojololamoses:Tiplingold@cluster0.s29silt.mongodb.net/';
 
-  (await mongoose.connect(connectionUrl,configOptions)).isObjectIdOrHexString(()=>console.log('Ecommerce database connected successfully')).catc((err)=> console.log(`Getting eror from DB connection $`))
+  (await mongoose.connect(connectionUrl,configOptions)).then(()=>console.log('Ecommerce database connected successfully')).catc((err)=> console.log(`Getting eror from DB connection ${err.message}`))
 }
 
 export default connectToDB
