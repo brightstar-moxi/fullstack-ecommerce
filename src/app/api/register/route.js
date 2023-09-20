ca56328f3a18e582311d109ea9eb672ce1d57600
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-20 02:08:50 +0100

    update

---
+++ src/app/api/register/route.js
@@ -0,0 +1,31 @@
+import connectToDB from "@/database";
+import Joi from "joi";
+import { NextResponse } from "next/server";
+
+
+const schema = Joi.object({
+    name : Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    role:Joi.string().required
+})
+
+export const dynamic = 'force-dynamic';
+
+export async function POST(req) {
+    
+    await connectToDB();
+
+    const {name, email, password, role} = await req.json();
+//validate the schema
+
+const {error} = schema.validate({name,email,password,role})
+
+if(error){
+    return NextResponse.json({
+        success : false,
+        message : email.details[0]
+    })
+}
+
+}
