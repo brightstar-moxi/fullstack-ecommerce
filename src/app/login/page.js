ace871297bb951589758d716070dcff32cd098db
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-25 05:05:28 +0100

    update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,22 +1,45 @@
 "use client"
 
 import InputComponent from "@/components/FormElement/InputComponent"
+import { GlobalContext } from "@/context"
+import { login } from "@/services/login"
 import { loginFormControls } from "@/utils"
+import { set } from "mongoose"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 const initialFormdata = {
-    email : "",
-    password : ""
+    email: "",
+    password: ""
 }
 
 export default function Login() {
 
-const [formData, SetFormData] = useState(initialFormdata)
+    const [formData, SetFormData] = useState(initialFormdata)
+    const {isAuthUser, setIsAuthUser, user, setUser} = useContext(GlobalContext)
 
     const router = useRouter();
 
     console.log(formData);
+
+    function isValidForm() {
+        return formData && formData.email && formData.email.trim() != '' &&
+            formData.password && formData.password.trim() != '' ? true : false
+    }
+
+async function handleLogin(){
+    const res = await login(formData);
+
+    console.log(res);
+
+    if(res.success){
+        setIsAuthUser(true);
+        setUser(res?.finalData?.user);
+        
+    }else {
+        setIsAuthUser(false)
+    }
+}
 
     return (
         <div className="bg-white relative">
@@ -36,29 +59,34 @@ const [formData, SetFormData] = useState(initialFormdata)
                                             placeholder={controlItem.placeholder}
                                             label={controlItem.label}
                                             value={formData[controlItem.id]}
-                                            onChange={(event)=> {
+                                            onChange={(event) => {
                                                 SetFormData({
                                                     ...formData,
-                                                    [controlItem.id] : event.target.value
+                                                    [controlItem.id]: event.target.value
                                                 })
                                             }}
-                                        /> 
-                                     ) :
-                                         null
+                                        />
+                                    ) :
+                                        null
                                     )
                                 }
-                                <button className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white transition-all duration-200 ease-in-out
-                             focus:shadow font-medium uppercase tracking-wide">
+                                <button
+                  className="disabled:opacity-50 inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg 
+                     text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase tracking-wide
+                     "
+                  disabled={!isValidForm()}
+                  onClick={handleLogin}
+                >
                                     Login
                                 </button>
                                 <div className="flex flex-col gap-2">
                                     <p>New to website ?</p>
                                     <button className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white transition-all duration-200 ease-in-out
                              focus:shadow font-medium uppercase tracking-wide"
-                             onClick={() => router.push("/register")}
-                             >
-                                    Register
-                                </button>
+                                        onClick={() => router.push("/register")}
+                                    >
+                                        Register
+                                    </button>
                                 </div>
                             </div>
 
--- src/context/index.js
+++ src/context/index.js
@@ -9,9 +9,13 @@ export const GlobalContext = createContext(null);
 
 
 export default function GlobalState({ children }) {
-    const [showNavModal, setShowNavModal] = useState(false)
+    const [showNavModal, setShowNavModal] = useState(false);
+    const [commonLoader, setCommonLoader] = useState(false);
+    const [isAuthUser, setIsAuthUser] = useState(null);
+    const [user, setUser] = useState(null);
+
 
     return (
-        <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>{children}</GlobalContext.Provider>
+        <GlobalContext.Provider value={{ showNavModal, setShowNavModal, isAuthUser, setIsAuthUser,commonLoader, setCommonLoader, user, setUser }}>{children}</GlobalContext.Provider>
     )
 }
