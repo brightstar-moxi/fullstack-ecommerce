e20abc69003dc2843c3796828359079a0b6ec190
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-26 04:44:25 +0100

    update

--- src/context/index.js
+++ src/context/index.js
@@ -10,12 +10,15 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
     const [showNavModal, setShowNavModal] = useState(false);
-    const [commonLoader, setCommonLoader] = useState(false);
+    const [componentLevelLoader, setComponentLevelLoader] = useState({
+        loading: false,
+        id: "",
+      });
     const [isAuthUser, setIsAuthUser] = useState(null);
     const [user, setUser] = useState(null);
 
 
     return (
-        <GlobalContext.Provider value={{ showNavModal, setShowNavModal, isAuthUser, setIsAuthUser,commonLoader, setCommonLoader, user, setUser }}>{children}</GlobalContext.Provider>
+        <GlobalContext.Provider value={{ showNavModal, setShowNavModal, isAuthUser, setIsAuthUser,componentLevelLoader, setComponentLevelLoader, user, setUser }}>{children}</GlobalContext.Provider>
     )
 }
--- src/services/login/index.js
+++ src/services/login/index.js
@@ -15,3 +15,4 @@ export const login = async (formData) => {
       console.log(error);
     }
   };
+  
