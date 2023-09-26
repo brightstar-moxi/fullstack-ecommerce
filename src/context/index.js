9ee8580de199342e2adde2dedc1b2556e37249d2
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-26 05:24:49 +0100

    update

--- src/context/index.js
+++ src/context/index.js
@@ -1,6 +1,7 @@
 "use client"
 
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import Cookies from "js-cookie";
 
 
 
@@ -17,6 +18,18 @@ export default function GlobalState({ children }) {
     const [isAuthUser, setIsAuthUser] = useState(null);
     const [user, setUser] = useState(null);
 
+
+useEffect(()=>{
+    console.log(Cookies.get('token'))
+
+    if(Cookies.get('token')!== undefined){
+        setIsAuthUser(true);
+        const userData = JSON.parse(localStorage.getItem('user')) || {};
+        setUser(userData)
+    } else {
+        setIsAuthUser(false)
+    }
+}, [Cookies])
 
     return (
         <GlobalContext.Provider value={{ showNavModal, setShowNavModal, isAuthUser, setIsAuthUser,componentLevelLoader, setComponentLevelLoader, user, setUser }}>{children}</GlobalContext.Provider>
