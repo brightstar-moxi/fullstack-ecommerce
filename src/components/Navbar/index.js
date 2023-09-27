e8b32d2cd0749bc7289b831dd48d372d503f0614
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-27 18:03:19 +0100

    updates

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,106 +1,110 @@
 "use client"
 
 import { GlobalContext } from "@/context";
-import { adminNavOptions,navOptions } from "@/utils";
+import { adminNavOptions, navOptions } from "@/utils";
 import { usePathname, useRouter } from "next/navigation";
 import { Fragment, useContext } from "react";
 import CommonModal from "../CommonModal";
 import Cookies from "js-cookie";
 
-const isAdminView = false;
+// const isAdminView = false;
 
 
-function NavItems({isModalView = false}) {
+function NavItems({ isModalView = false, isAdminView }) {
 
-    return (
-        // <div className={`items-center justify-between w-full md:flex md:w-auto ${isModalView ? "" : "hidden"}`} id="nav-items">
-        <div
-        className={`items-center justify-between w-full md:flex md:w-auto ${
-          isModalView ? "" : "hidden"
+  return (
+    // <div className={`items-center justify-between w-full md:flex md:w-auto ${isModalView ? "" : "hidden"}`} id="nav-items">
+    <div
+      className={`items-center justify-between w-full md:flex md:w-auto ${isModalView ? "" : "hidden"
         }`}
-        id="nav-items"
-      >
+      id="nav-items"
+    >
 
-            <ul className={`flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${
-          isModalView ? "border-none" : "border border-gray-100"
+      <ul className={`flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${isModalView ? "border-none" : "border border-gray-100"
         }`}>
-                {
-                    isAdminView ? adminNavOptions.map((item) => (<li
-                        className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
-                        key={item.id}>
-                            {item.label}
-                        </li>))
-                        : navOptions.map((item) => (
-                            <li
-                              className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
-                              key={item.id}
-                            //   onClick={() => router.push(item.path)}
-                            >
-                              {item.label}
-                            </li>
-                          ))}
-            </ul>
-        </div>
-    )
+        {
+          isAdminView ? adminNavOptions.map((item) => (<li
+            className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+            key={item.id}>
+            {item.label}
+          </li>))
+            : navOptions.map((item) => (
+              <li
+                className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+                key={item.id}
+              //   onClick={() => router.push(item.path)}
+              >
+                {item.label}
+              </li>
+            ))}
+      </ul>
+    </div>
+  )
 }
 
 export default function Navbar() {
 
-    const { showNavModal, setShowNavModal } = useContext(GlobalContext);
-  
-    const { user,
-      isAuthUser,
-      setIsAuthUser,
-      setUser,
-      currentUpdatedProduct,
-      setCurrentUpdatedProduct,
-      showCartModal,
-      setShowCartModal} = useContext(GlobalContext);
+  const { showNavModal, setShowNavModal } = useContext(GlobalContext);
 
-      const pathName = usePathname();
-    const router = useRouter()
+  const { user,
+    isAuthUser,
+    setIsAuthUser,
+    setUser,
+    currentUpdatedProduct,
+    setCurrentUpdatedProduct,
+    showCartModal,
+    setShowCartModal } = useContext(GlobalContext);
 
-    console.log(pathName)
+  const pathName = usePathname();
+  const router = useRouter()
 
-    function handleLogout(){
-      setIsAuthUser(false)
-      setUser(null)
-      Cookies.remove('token')
-      localStorage.clear()
-      router.push('/')
-    }
+  console.log(pathName)
 
-    return (
-        <>
-            <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <div className="flex items-center cursor-pointer">
-                        <span className="slef-center text-2xl font-semibold whitespace-nowrap">
-                            Brightstar-Ecommerce
-                        </span>
+  function handleLogout() {
+    setIsAuthUser(false)
+    setUser(null)
+    Cookies.remove('token')
+    localStorage.clear()
+    router.push('/')
+  }
 
-                    </div>
+  const isAdminView = pathName.includes("admin-view")
+  return (
+    <>
+      <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div onClick={()=>router.push('/')} className="flex items-center cursor-pointer">
+            <span className="slef-center text-2xl font-semibold whitespace-nowrap">
+              Brightstar-Ecommerce
+            </span>
 
-                    <div className="flex md:order-2 gap-2 ">
-                        {!isAdminView && isAuthUser ? (
-                            <Fragment>
-                                <button  className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Account</button>
-                                <button className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Cart</button>
-                            </Fragment>
-                        ) : null}
-                        {
-                            user?.role === 'admin' ?
-                                isAdminView ? (
-                                <button className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Client View</button>)
-                                 : (
-                                 <button onClick={()=> router.push('/admin-view')} className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Admin Veiw</button>)
-                                : null
-                        }
-                        {
-                            isAuthUser ? (<button onClick={handleLogout} className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white" >Logout</button>) : 
-                            (<button onClick={()=> router.push('/login')} className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Login</button>)
-                        }
-                           <button
+          </div>
+
+          <div className="flex md:order-2 gap-2 ">
+            {!isAdminView && isAuthUser ? (
+              <Fragment>
+                <button className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Account</button>
+                <button className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Cart</button>
+              </Fragment>
+            ) : null}
+            {
+              user?.role === 'admin' ?
+                isAdminView ? (
+                  <button className={"mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white"}
+                    onClick={() => router.push('/')}
+                  >Client View</button>
+                )
+                  : (
+                    <button onClick={() => router.push('/admin-view')} className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Admin Veiw</button>)
+                : null
+            }
+            {
+              isAuthUser ? (
+                <button onClick={handleLogout} className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white" >Logout</button>) :
+                (
+                  <button onClick={() => router.push('/login')} className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white">Login</button>)
+            }
+            <button
               data-collapse-toggle="navbar-sticky"
               type="button"
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -123,17 +127,17 @@ export default function Navbar() {
                 ></path>
               </svg>
             </button>
-         
-                    </div>
-                    <NavItems />
-                </div>
+
+          </div>
+          <NavItems isAdminView={isAdminView} />
+        </div>
 
 
-            </nav>
-            <CommonModal
-            showModalTitle={false}
-            mainContent={<NavItems isModalView={true}/>}
-             show={showNavModal} setShow={setShowNavModal}/>
-        </>
-    )
+      </nav>
+      <CommonModal
+        showModalTitle={false}
+        mainContent={<NavItems isModalView={true} isAdminView={isAdminView} />}
+        show={showNavModal} setShow={setShowNavModal} />
+    </>
+  )
 }
