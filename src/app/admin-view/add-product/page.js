0fc4d72af7010ea2b51215e7727436bf55b1bbed
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-29 04:52:10 +0100

    update

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -4,27 +4,44 @@ import InputComponent from "@/components/FormElement/InputComponent"
 import SelectComponent from "@/components/FormElement/SelectComponent"
 import TileComponent from "@/components/FormElement/TileComponent"
 import { adminAddProductformControls, AvailableSizes, firebaseConfig, firebaseStroageURL } from "@/utils"
-import {initializeApp} from 'firebase/app'
-import {getStorage} from 'firebase/storage'
+import { initializeApp } from 'firebase/app'
+import { getDownloadURL, getStorage, uploadBytesResumable, ref } from 'firebase/storage'
+import { resolve } from "styled-jsx/css"
 
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app, firebaseStroageURL)
 
-const createUniqueFileName= (getFile)=>{
+const createUniqueFileName = (getFile) => {
+    const timeStamp = Date.now();
+    const randomStringValue = Math.random().toString(36).substring(2, 12);
 
+
+    return `${getFile.name}-${timeStamp}-${randomStringValue}`
 }
 
-async function helperForUPloadingImageToFirebase(files){
+async function helperForUPloadingImageToFirebase(file) {
+    const getFileName = createUniqueFileName(file);
+    const storageReference = ref(storage, `ecommerce/${getFileName}`);
+    const uploadImage = uploadBytesResumable(storageReference, file);
 
+
+    return new Promise((resolve, reject) => {
+        uploadImage.on('state_changed', (snapshot) => { }, (error) => {
+            console.log(error)
+            reject(error)
+        }, () => {
+            getDownloadURL(uploadImage.snapshot.ref).then(downloadUrl => resolve(downloadUrl)).catch(error => reject(error))
+        })
+    })
 }
 
 export default function AdminAddNewProduct() {
 
-  async function handleImage(event) {
-console.log(event.target.files);
-const extractImageUrl = await helperForUPloadingImageToFirebase(event.target.files[0])
-console.log(extractImageUrl)
+    async function handleImage(event) {
+        console.log(event.target.files);
+        const extractImageUrl = await helperForUPloadingImageToFirebase(event.target.files[0])
+        console.log(extractImageUrl)
     }
 
     return (
