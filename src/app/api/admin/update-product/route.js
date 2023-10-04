f550eebf8c60e241e760e69ab4c22ac939636b81
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-10-04 04:24:46 +0100

    updated

---
+++ src/app/api/admin/update-product/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import connectToDB from "@/database";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req) {
+    try {
+        await connectToDB();
+        const extractData = await req.json();
+
+        const {
+            _id,
+            name, price,
+            description, category,
+            sizes, deliveryInfo, onSale, priceDrop, imageUrl
+        } = extractData
+
+        const updatedProduct = await Product.findOneAndUpdate({
+            _id: _id
+        }, {
+            name, price,
+            description, category,
+            sizes, deliveryInfo, onSale, priceDrop, imageUrl
+        }, { new: true }
+        );
+        if(updatedProduct){
+            return NextResponse.json({
+                success : false,
+                message: "something went wrong ! Please try again later"
+            })
+        }
+
+    } catch (e) {
+        console.log(error);
+        return NextResponse.json({
+            success: false,
+            message: "something went wrong ! Please try again later",
+        });
+    }
+}
--- src/components/FormElement/SelectComponent/index.js
+++ src/components/FormElement/SelectComponent/index.js
@@ -4,12 +4,12 @@ export default function SelectComponent({ label, value, onChange, options = [] }
 
     return (
         <div className="relative">
-            <p className="pt-0 pr-2 absolute pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-blue-600 bg-white">
+            <p className="pt-0 pr-2 absolute pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-black-600 bg-white">
                 {label}
             </p>
             <select value={value}
                 onChange={onChange}
-                className="border placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mr-0 mt-0 ml-0 text-base block bg-white border-gray-300 rounded-md"
+                className="border placeholder-blue-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mr-0 mt-0 ml-0 text-base block bg-white border-blue-300 rounded-md"
             >
                 {
                     options && options.length ?
