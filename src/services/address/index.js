b7fad882223b5ae902dae970dce5935c9f9f7947
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-10-25 05:09:38 +0100

    updates

---
+++ src/services/address/index.js
@@ -0,0 +1,32 @@
+
+
+export const addNewAddress = async(formdata)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const fetchAllAddresses = async(formdata)=>{
+    try {
+        const res = await fetch('/api/address/ add-new-address')
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const updateAddress = async(formdata)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const deleteAddress = async(id)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
