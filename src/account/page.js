b5c1cc38ce5fcfaa969eedf3155753c8e0849604
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-10-25 14:41:53 +0100

    services update

---
+++ src/account/page.js
@@ -0,0 +1,9 @@
+"use client"
+export default function Accoint(){
+
+    return (
+        <>
+        
+        </>
+    )
+}
--- src/services/address/index.js
+++ src/services/address/index.js
@@ -1,31 +1,76 @@
+import Cookies from "js-cookie";
 
 
-export const addNewAddress = async(formdata)=>{
+export const addNewAddress = async (formdata) => {
     try {
-        
+        const res = await fetch('/api/address/add-new-address', {
+            method: 'POST',
+            headers: {
+                'Content-Type': ' application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+            body: JSON.stringify(formdata)
+        })
+
+        const data = await res.json()
+
+        return data;
+
     } catch (error) {
         console.log(error);
     }
 }
 
 
-export const fetchAllAddresses = async(formdata)=>{
+export const fetchAllAddresses = async (id) => {
     try {
-        const res = await fetch('/api/address/ add-new-address')
+        const res = await fetch(`/api/address/get-all-address?id=${id}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+            body: JSON.stringify(formdata)
+        })
+
+        const data = await res.json()
+
+        return data;
     } catch (error) {
         console.log(error);
     }
 }
-export const updateAddress = async(formdata)=>{
+export const updateAddress = async (formdata) => {
     try {
-        
+        const res = await fetch('/api/address/update-address', {
+            headers: {
+                'Content-Type': ' application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+            body: JSON.stringify(formdata),
+        })
+
+        const data = await res.json()
+
+        return data;
+
+
     } catch (error) {
         console.log(error);
     }
 }
-export const deleteAddress = async(id)=>{
+export const deleteAddress = async (id) => {
     try {
-        
+        const res = await fetch('/api/address/delete-address', {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${Cookies.get('token')}`
+            },
+        })
+
+        const data = await res.json()
+
+        return data ;
+
     } catch (error) {
         console.log(error);
     }
