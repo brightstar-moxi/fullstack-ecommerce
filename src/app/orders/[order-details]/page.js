256218f591279f8ba71275c53477df37608a9b1a
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-11-13 01:18:18 +0100

    order updated

--- src/app/orders/[order-details]/page.js
+++ src/app/orders/[order-details]/page.js
@@ -48,6 +48,22 @@ export default function OrderDetail() {
         <div className="py-14 px-4 md:px-6">
             <div className="flex justify-start items-start space-y-2 flex-col">
                 <h1 className="text-3xl lg:text-4xl font-bold leading-7 lg:leading-9 text-gray-900">Order #{orderDetails._id}</h1>
+           <p className="text-base font-medium leading-6 text-gray-600">
+            {orderDetails &&
+            orderDetails.createdAt &&
+            orderDetails.createdAt.split("T")[0]}{" "}
+          |{" "}
+          {orderDetails &&
+            orderDetails.createdAt &&
+            orderDetails.createdAt.split("T")[1].split(".")[0]
+
+            }
+           </p>
+            </div>
+            <div className="mt-10 flex flex-col justify-center xl:flex-row items-stretch w-full xl:space-x-8 md:space-y-6 xl:space-y-0">
+                <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
+                    <div className="flex flex-col justify-start items-start bg-gray-50 px-4 py-4 md:p-6 xl:p-8 w-full"></div>
+                </div>
             </div>
         </div>
     )
