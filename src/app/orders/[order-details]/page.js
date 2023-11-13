9445eab5f66ea2fafeabb2745de82b908ecda790
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-11-13 04:16:20 +0100

    order details updated

--- src/app/orders/[order-details]/page.js
+++ src/app/orders/[order-details]/page.js
@@ -48,21 +48,71 @@ export default function OrderDetail() {
         <div className="py-14 px-4 md:px-6">
             <div className="flex justify-start items-start space-y-2 flex-col">
                 <h1 className="text-3xl lg:text-4xl font-bold leading-7 lg:leading-9 text-gray-900">Order #{orderDetails._id}</h1>
-           <p className="text-base font-medium leading-6 text-gray-600">
-            {orderDetails &&
-            orderDetails.createdAt &&
-            orderDetails.createdAt.split("T")[0]}{" "}
-          |{" "}
-          {orderDetails &&
-            orderDetails.createdAt &&
-            orderDetails.createdAt.split("T")[1].split(".")[0]
+                <p className="text-base font-medium leading-6 text-gray-600">
+                    {orderDetails &&
+                        orderDetails.createdAt &&
+                        orderDetails.createdAt.split("T")[0]}{" "}
+                    |{" "}
+                    {orderDetails &&
+                        orderDetails.createdAt &&
+                        orderDetails.createdAt.split("T")[1].split(".")[0]
 
-            }
-           </p>
+                    }
+                </p>
             </div>
             <div className="mt-10 flex flex-col justify-center xl:flex-row items-stretch w-full xl:space-x-8 md:space-y-6 xl:space-y-0">
                 <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
-                    <div className="flex flex-col justify-start items-start bg-gray-50 px-4 py-4 md:p-6 xl:p-8 w-full"></div>
+                    <div className="flex flex-col justify-start items-start bg-gray-50 px-4 py-4 md:p-6 xl:p-8 w-full">
+                        <p className="font-bold text-lg ">Your order summary</p>
+
+                        {orderDetails &&
+                            orderDetails.orderItems &&
+                            orderDetails.orderItems.length
+                            ? orderDetails.orderItems.map((item) => (
+                                <div key={item._id} className="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full ">
+                                    <div className="pb-4 md:pb-8 w-full md:w-40" >
+                                        <img
+                                            src={item && item.product && item.product.imageUrl}
+                                            className="w-full hidden md:block"
+                                        />
+                                    </div>
+                                    <div className="border-b border-gray-300 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
+                                        <div className="w-full flex flex-col justify-start items-start space-y-8">
+                                            < h3 className="text-xl font-semibold loading-6 text-gray-900">
+                                                {item && item.product && item.product.name}
+                                            </h3>
+                                        </div>
+                                        <div className="w-full flex justify-between items-start space-x-8">
+                                            < h3 className="text-xl font-semibold loading-6 text-gray-900">
+                                                {item && item.product && item.product.price}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                            : null
+                        }
+                    </div>
+                    <div className="flex justify-center flex-col md:flex-row items-stretch w-full space-y-4 md:space-y-0 md:space-x-5 xl:space=x-8">
+                        <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6">
+                            <h3  className="text-xl font-semibold loading-6 text-gray-900">Summary </h3>
+                            <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
+                                <div className="flex justify-between w-full">
+                                    <p className="text-base leading-5 text-gray-800">Subtotal</p>
+                                    <p className="text-base leading-5 text-gray-500">${orderDetails && orderDetails.totalPrice}</p>
+                                </div>
+                                <div className="flex justify-between w-full">
+                                    <p className="text-base leading-5 text-gray-800">Shipping</p>
+                                    <p className="text-base leading-5 text-gray-900">Free</p>
+                                </div>
+                                <div className="flex justify-between w-full">
+                                    <p className="text-base leading-5 text-gray-800">Subtotal</p>
+                                    <p className="text-base leading-5 text-gray-500">${orderDetails && orderDetails.totalPrice}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
