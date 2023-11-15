0d0a04db57bbc06302d6fbe05f3fc165a8baf86b
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-11-15 02:17:59 +0100

    order details update

--- src/app/orders/[order-details]/page.js
+++ src/app/orders/[order-details]/page.js
@@ -8,7 +8,7 @@ import { PulseLoader } from "react-spinners";
 
 export default function OrderDetail() {
 
-    const { pageLevelLoader, setPageLevelLoader, orderDetails, setOrderDetails } = useContext(GlobalContext);
+    const { pageLevelLoader, setPageLevelLoader, orderDetails, setOrderDetails, user } = useContext(GlobalContext);
 
     const params = useParams()
 
@@ -95,7 +95,7 @@ export default function OrderDetail() {
                     </div>
                     <div className="flex justify-center flex-col md:flex-row items-stretch w-full space-y-4 md:space-y-0 md:space-x-5 xl:space=x-8">
                         <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6">
-                            <h3  className="text-xl font-semibold loading-6 text-gray-900">Summary </h3>
+                            <h3 className="text-xl font-semibold loading-6 text-gray-900">Summary </h3>
                             <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
                                 <div className="flex justify-between w-full">
                                     <p className="text-base leading-5 text-gray-800">Subtotal</p>
@@ -109,26 +109,38 @@ export default function OrderDetail() {
                                     <p className="text-base leading-5 text-gray-800">Total</p>
                                     <p className="text-base leading-5 text-gray-500">${orderDetails && orderDetails.totalPrice}</p>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-                    <div className="bg-gray-50 w-full xl:w-96 flex  items-center md:items-start px-4 py-6 flex-col">
-            <h3 className="text-xl font-semibold leading-6 text-gray-900">
-              Customer Details
-            </h3>
-            <div className="flex flex-col justify-start items-start flex-shrink-0">
-              <div className="flex gap-4 justify-center flex-col w-full md:justify-start   py-8 border-b border-gray-200">
-                <p className="text-base font-semibold leading-4 text-left text-gray-950">
-                  Name: {user?.name}
-                </p>
-                <p className="text-base font-semibold leading-4 text-left text-gray-950">
-                  Email: {user?.email}
-                </p>
-              </div>
-            </div>
-            </div>
-            </div>
+                </div>
+
+                <div className="bg-gray-50 w-full xl:w-96 flex items-center md:items-start px-4 py-6 flex-col">
+                    <h3 className="text-xl font-semibold leading-6 text-gray-900">
+                        Customer Details
+                    </h3>
+                    <div className="flex flex-col justify-start items-start flex-shrink-0">
+                        <div className="flex gap-4 justify-center flex-col w-full md:justify-start py-8 border-b border-gray-200">
+                            <p className="text-base font-semibold leading-4 text-left text-gray-950">
+                                Name: {user?.name}
+                            </p>
+                            <p className="text-base font-semibold leading-4 text-left text-gray-950">
+                                Email: {user?.email}
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
+                    <div className="flex justify-center md:justify-start xl:flex-col md:space-x-6 lg:space-x-9 space-x-4 md:space-y-0 xl:space-y-12 md:flex-row item-center md:items-start">
+                        <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
+                            <p>Shipping Address</p>
+                            <p>{orderDetails && orderDetails.shippingAddress.address}</p>
+                            <p>{orderDetails && orderDetails.shippingAddress.}</p>
+                            <p>{orderDetails && orderDetails.shippingAddress.address}</p>
+                            <p>Shipping Address</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
