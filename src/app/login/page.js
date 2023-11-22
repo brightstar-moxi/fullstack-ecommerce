084c1366aa36054e79385c429054c541905d5f91
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-11-22 15:12:54 +0100

    Bright

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -97,13 +97,13 @@ if (isAuthUser) router.push("/client-view") ;
         <div className="bg-blue-700 relative">
             <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto xl:px-5 lg:flex-row ">
                 <div className="flex flex-col justify-center items-center w-full pr-10 pl-10 lg:flex-row ">
-                    <div className="md:w-full mt-10 mr-0 mb-0 relative max-w-2xl lg:mt-0 lg:w-5/12 ">
-                        <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 p-20 bg-white shadow-2xl rounded-xl relative z-10">
-                            <p className=" text-4xl font-medium text-center font-serif text-blue-700">
+                    <div className="md:w-full  mt-1 mr-0 mb-0 relative max-w-2xl lg:mt-0 lg:w-5/12 ">
+                        <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 p-20 bg-white md:w-full  shadow-2xl rounded-xl relative z-10">
+                            <p className="w-full text-4xl font-medium text-center font-serif text-blue-700">
                                 Login
                             </p>
 
-                            <div className="w-full md:w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
+                            <div className="w-full md:w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8 ">
                                 {
                                     loginFormControls.map(controlItem => controlItem.componentType === 'input' ? (
                                         <InputComponent
