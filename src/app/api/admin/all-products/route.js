ace4d98c8c3e7eb84da46d4c6d7838d204e6b527
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-11-22 12:51:40 +0100

    product error solved

--- src/app/api/admin/all-products/route.js
+++ src/app/api/admin/all-products/route.js
@@ -1,3 +1,36 @@
+// import connectToDB from "@/database";
+// import Product from "@/models/product";
+// import { NextResponse } from "next/server";
+
+// export const dynamic = "force-dynamic";
+
+// export async function GET(req) {
+//   try {
+//     await connectToDB();
+
+//       const extractAllproducts = await Product.find({});
+
+//       if (extractAllproducts) {
+//         return NextResponse.json({
+//           success: true,
+//           data: extractAllproducts,
+//         });
+//       } else {
+//         return NextResponse.json({
+//           success: false,
+//           status: 204,
+//           message: "No Products found",
+//         });
+//       }
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json({
+//       success: false,
+//       message: "Something went wrong ! Please try again later",
+//     });
+//   }
+// }
+
 import connectToDB from "@/database";
 import Product from "@/models/product";
 import { NextResponse } from "next/server";
@@ -6,27 +39,30 @@ export const dynamic = "force-dynamic";
 
 export async function GET(req) {
   try {
+    // Ensure the database connection is established
     await connectToDB();
 
-      const extractAllproducts = await Product.find({});
+    // Retrieve all products
+    const extractAllproducts = await Product.find({});
 
-      if (extractAllproducts) {
-        return NextResponse.json({
-          success: true,
-          data: extractAllproducts,
-        });
-      } else {
-        return NextResponse.json({
-          success: false,
-          status: 204,
-          message: "No Products found",
-        });
-      }
+    // Check if the array is not empty
+    if (extractAllproducts.length > 0) {
+      return NextResponse.json({
+        success: true,
+        data: extractAllproducts,
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        status: 204,
+        message: "No Products found",
+      });
+    }
   } catch (error) {
     console.log(error);
     return NextResponse.json({
       success: false,
-      message: "Something went wrong ! Please try again later",
+      message: "Something went wrong! Please try again later",
     });
   }
 }
